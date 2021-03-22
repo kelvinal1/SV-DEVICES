34190eb06182e6b8b83d78973744e7ca0287ab75
Kevin <Kevin@hp>
2021-03-21 22:23:12 -0500

    Inicializando repositorio en github

---
+++ src/modelo/ConexionPG.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.postgresql.*;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Kevin
+ */
+public class ConexionPG {
+
+    private String url = "jdbc:postgresql://localhost:5433/SV-DEVICES";
+    private String usuario = "postgres";
+    private String clave = "1234";
+    private Connection con;
+
+    public ConexionPG() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConexionPG.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            con=DriverManager.getConnection(url, usuario, clave);
+            System.out.println("BASE ENLAZADA");
+                    
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionPG.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public ResultSet Query(String slq) {
+        try {
+            Statement st;
+            st = con.createStatement();
+            return st.executeQuery(slq);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionPG.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+
+    }
+    
+    
+    public SQLException NoQuery(String sql){
+        
+        try {
+            Statement st;
+            st=con.createStatement();
+            st.execute(sql);
+            return null;
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionPG.class.getName()).log(Level.SEVERE, null, ex);
+            return ex;
+        }
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+    
+    
+}
+
